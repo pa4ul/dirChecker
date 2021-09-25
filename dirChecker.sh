66b6dcd974a3baf2732854d606700adfb955eb10
paulh <paulherbich1@gmail.com>
2021-09-26 00:26:32 +0200

    Basic function finished

--- dirChecker.sh
+++ dirChecker.sh
@@ -1,15 +1,58 @@
 #!bin/bash
 echo -e "Welcome to $0."
 read -p "Enter the absolute path of you given directory: " path
-echo -e "\nHere you can see files with write rights\n"
+echo -e "\n"
 
-outputRaw=($(ls -la $path | awk '{ print $1 "\t" $9}'))
-#echo ${outputRaw[*]} -> to display the whole array
-for ((i=1; i<=${#outputRaw[@]}; i+=2));
-do
-if [[ ${outputRaw[i]:1:2} = "rw" ]]
-then
-#echo -e ${outputRaw[i]}"\t"${outputRaw[i+1]} -> lists all filenames INCLUSIVE their rights
-echo ${outputRaw[i+1]}
-fi
-done
+echo -e "Additinal options available"
+echo -e "\t1) List everything"
+echo -e "\t2) List only Files"
+echo -e "\t3) List only Directorys"
+
+function listEverything {
+    outputRaw=($(ls -la $path | awk '{ print $1 "\t" $9}'))
+    #echo ${outputRaw[*]} -> to display the whole array
+    for ((i=1; i<=${#outputRaw[@]}; i+=2));
+        do
+        if [[ ${outputRaw[i]:1:2} = "rw" ]]
+            then
+            #echo -e ${outputRaw[i]}"\t"${outputRaw[i+1]} -> lists all filenames INCLUSIVE their rights
+            echo ${outputRaw[i+1]}
+        fi
+    done
+}
+
+function listFiles {
+    outputRaw=($(ls -la $path | awk '{ print $1 "\t" $9}'))
+    #echo ${outputRaw[*]} -> to display the whole array
+    for ((i=1; i<=${#outputRaw[@]}; i+=2));
+        do
+        if [[ ${outputRaw[i]:0:3} = "-rw" ]]
+            then
+            echo ${outputRaw[i+1]}
+            #echo ${outputRaw[i]:0:3}
+        fi
+    done
+}
+
+function listDirectorys {
+    outputRaw=($(ls -la $path | awk '{ print $1 "\t" $9}'))
+    #echo ${outputRaw[*]} -> to display the whole array
+    for ((i=1; i<=${#outputRaw[@]}; i+=2));
+        do
+        if [[ ${outputRaw[i]:0:3} = "drw" ]]
+            then
+            echo ${outputRaw[i+1]}
+        fi
+    done
+}
+
+read -p "Select your option: " opt
+echo -e "\n"
+
+case $opt in
+"1") listEverything ;;
+"2") listFiles ;;
+"3") listDirectorys ;;
+esac
+#echo -e "\nHere you can see files with write rights\n"
+
