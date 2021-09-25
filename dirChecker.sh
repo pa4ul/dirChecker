c516e2e45c5dfcae49ec6f7258aebb12435182f8
paulh <paulherbich1@gmail.com>
2021-09-25 14:57:52 +0200

    Before implementing loop

--- dirChecker.sh
+++ dirChecker.sh
@@ -1,8 +1,13 @@
 #!bin/bash
-
 echo -e "Welcome to $0."
 read -p "Enter the absolute path of you given directory: " path
 echo -e "\nHere you can see files with write rights\n"
 
-outputRaw=($(ls -l $path | awk '{ print $1 "\t" $9}'))
-echo ${outputRaw[*]}
+outputRaw=($(ls -la $path | awk '{ print $1 "\t" $9}'))
+#echo ${outputRaw[*]} -> to display the whole array
+for ((i=1; i<=${#outputRaw[@]}; i+=2));
+do
+#if [[ $ ]]
+echo ${outputRaw[i]}
+
+done
